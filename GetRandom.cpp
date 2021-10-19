ba2d78f7e4224ae6eb16401d68f6d6a3940b414e
Heartaster <406321927@qq.com>
2021-10-19 13:49:57 +0800

    Revert "adding command"
    This reverts commit fced34b9cc71318c590f1d3182eef25d6b89f95f.

--- GetRandom.cpp
+++ GetRandom.cpp
@@ -5,18 +5,20 @@
 using namespace std;
 
 void getRandom(int grade, double arr[3]) {
-    //定义随机数种子
+    //定义默认随机数引擎
     srand(time(NULL));
-//根据不同的输入年级生成计算题的两个组成数
+
     if (grade == 1 || grade == 2) {
         arr[0] = rand() % 101;
         arr[1] = rand() % 101;
         arr[2] = rand() % 2;
     } else if (grade == 3 || grade == 4) {
+
         arr[0] = rand() % 101;
         arr[1] = rand() % 101;
         arr[2] = rand() % 4;
     } else if (grade == 5 || grade == 6) {
+
         arr[0] = 100 * rand() / double(RAND_MAX);
         arr[1] = 100 * rand() / double(RAND_MAX);
         arr[2] = rand() % 4;
